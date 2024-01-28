857e78c020ac67bfed73506239f1b6a00659c727
Naresh <nareshargela200@gmail.com>
2024-01-28 20:47:42 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+"mongodb+srv://User:User55@user.h3b3ogz.mongodb.net/BookStore";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
